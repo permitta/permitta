9781b01dc061ae830d70dad4fa70483220100cb8
Joel Coenraadts <joel@bytelab.com>
2024-03-23 15:55:08 +1100

    working simple implementation with opa

--- app/main.py
+++ app/main.py
@@ -4,19 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from app.config import Settings
 from app.routes import router
 
-from fastapi_opa import OPAConfig
-from fastapi_opa import OPAMiddleware
-from fastapi_opa.auth.auth_api_key import APIKeyConfig, APIKeyAuthentication
-
-opa_host = "http://localhost:8181"
-
-# Configure API keys
-api_key_config = APIKeyConfig(
-    header_key="test",
-    api_key="1234"
-)
-api_key_auth = APIKeyAuthentication(api_key_config)
-opa_config = OPAConfig(authentication=api_key_auth, opa_host=opa_host)
+use_opa: bool = True
 
 settings = Settings()
 
@@ -27,7 +15,23 @@ def get_app() -> FastAPI:
 
 
 app = get_app()
-app.add_middleware(OPAMiddleware, config=opa_config)
+
+if use_opa:
+    from fastapi_opa import OPAConfig
+    from fastapi_opa import OPAMiddleware
+    from fastapi_opa.auth.auth_api_key import APIKeyConfig, APIKeyAuthentication
+
+    opa_host = "http://localhost:8181"
+    # Configure API keys
+    api_key_config = APIKeyConfig(
+        header_key="test",
+        api_key="1234"
+    )
+    api_key_auth = APIKeyAuthentication(api_key_config)
+    opa_config = OPAConfig(authentication=api_key_auth, opa_host=opa_host)
+    app.add_middleware(OPAMiddleware, config=opa_config)
+
+
 app.mount("/", StaticFiles(directory="app/static/dist"), name="static")
 
 
