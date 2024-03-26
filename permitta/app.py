b95794854c1453f66003ea455fe101a07ddb44d7
Joel Coenraadts <joel@bytelab.com>
2024-03-26 23:40:39 +1100

    added session wrapper

--- permitta/app.py
+++ permitta/app.py
@@ -1,6 +1,3 @@
-import json
-import logging
-
 from flask import Flask, session, jsonify
 from flask_pyoidc.provider_configuration import ProviderConfiguration, ClientMetadata
 from flask_pyoidc import OIDCAuthentication
@@ -9,7 +6,9 @@ from flask_pyoidc.user_session import UserSession
 PROVIDER_NAME: str = "keycloak"
 provider_config: ProviderConfiguration = ProviderConfiguration(
     issuer="http://localhost:8080/realms/permitta",
-    client_metadata=ClientMetadata(client_id="permitta-client", client_secret="AuSlZ8hJoEPcwX6jjTsIx6JXHIYbZLkE"),
+    client_metadata=ClientMetadata(
+        client_id="permitta-client", client_secret="AuSlZ8hJoEPcwX6jjTsIx6JXHIYbZLkE"
+    ),
 )
 
 app = Flask(__name__)
@@ -20,11 +19,26 @@ auth = OIDCAuthentication({PROVIDER_NAME: provider_config})
 auth.init_app(app)
 
 
+class PermittaSession(UserSession):
+    @property
+    def given_name(self) -> str:
+        return self.userinfo.get("given_name")
+
+    @property
+    def family_name(self) -> str:
+        return self.userinfo.get("family_name")
+
+    @property
+    def email(self) -> str:
+        return self.userinfo.get("email")
+
+
 @app.route("/")
 @auth.oidc_auth(PROVIDER_NAME)
 def index():
-    user_session = UserSession(session)
-    return "ok"
+    permitta_session = PermittaSession(session)
+    return f"Hello {permitta_session.given_name} {permitta_session.family_name} with email {permitta_session.email}"
+
 
 @app.route("/hello")
 @auth.oidc_auth(PROVIDER_NAME)
@@ -32,16 +46,17 @@ def hello():
     user_session = UserSession(session)
     return "hello"
 
+
 @app.route("/logout")
 @auth.oidc_logout
 def logout():
-    # user_session = UserSession(session)
-    return "logged out  "
+    return "logged out"
+
 
 @app.route("/healthz")
 def healthz():
-    # user_session = UserSession(session)
-    return "logged out  "
+    return "healthy"
+
 
 @auth.error_view
 def error(error=None, error_description=None):
